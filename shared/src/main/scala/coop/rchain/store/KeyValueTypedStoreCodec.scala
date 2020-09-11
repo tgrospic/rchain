8a65c6a0b9b77e3a2c03ba64cc52d83707194dd6
Tomislav Grospic <grospic@gmail.com>
2020-09-11 17:23:47 +0200

    Use ByteVector when reading/writing to LMDB

--- shared/src/main/scala/coop/rchain/store/KeyValueTypedStoreCodec.scala
+++ shared/src/main/scala/coop/rchain/store/KeyValueTypedStoreCodec.scala
@@ -4,7 +4,7 @@ import cats.effect.Sync
 import cats.syntax.all._
 import coop.rchain.shared.ByteVectorOps.RichByteVector
 import scodec.Codec
-import scodec.bits.BitVector
+import scodec.bits.ByteVector
 
 class KeyValueTypedStoreCodec[F[_]: Sync, K, V](
     store: KeyValueStore[F],
@@ -12,33 +12,33 @@ class KeyValueTypedStoreCodec[F[_]: Sync, K, V](
     vCodec: Codec[V]
 ) extends KeyValueTypedStore[F, K, V] {
   // TODO: create specialized exceptions for Codec errors
-  def encodeKey(key: K): F[BitVector] =
+  def encodeKey(key: K): F[ByteVector] =
     kCodec
       .encode(key)
       .fold(
-        err => new Exception(err.message).raiseError[F, BitVector],
-        _.pure[F]
+        err => new Exception(err.message).raiseError[F, ByteVector],
+        _.toByteVector.pure[F]
       )
 
-  def decodeKey(bytes: BitVector): F[K] =
+  def decodeKey(bytes: ByteVector): F[K] =
     kCodec
-      .decodeValue(bytes)
+      .decodeValue(bytes.toBitVector)
       .fold(
         err => new Exception(err.message).raiseError[F, K],
         _.pure[F]
       )
 
-  def encodeValue(value: V): F[BitVector] =
+  def encodeValue(value: V): F[ByteVector] =
     vCodec
       .encode(value)
       .fold(
-        err => new Exception(err.message).raiseError[F, BitVector],
-        _.pure[F]
+        err => new Exception(err.message).raiseError[F, ByteVector],
+        _.toByteVector.pure[F]
       )
 
-  def decodeValue(bytes: BitVector): F[V] =
+  def decodeValue(bytes: ByteVector): F[V] =
     vCodec
-      .decodeValue(bytes)
+      .decodeValue(bytes.toBitVector)
       .fold(
         err => new Exception(err.message).raiseError[F, V],
         _.pure[F]
@@ -50,8 +50,8 @@ class KeyValueTypedStoreCodec[F[_]: Sync, K, V](
   override def get(keys: Seq[K]): F[Seq[Option[V]]] =
     for {
       keysBitVector <- keys.toVector.traverse(encodeKey)
-      keysBuf       = keysBitVector.map(_.toByteVector.toDirectByteBuffer)
-      valuesBytes   <- store.get(keysBuf, BitVector(_))
+      keysBuf       = keysBitVector.map(_.toDirectByteBuffer)
+      valuesBytes   <- store.get(keysBuf, ByteVector.view)
       values        <- valuesBytes.toVector.traverse(_.traverse(decodeValue))
     } yield values
 
@@ -60,28 +60,28 @@ class KeyValueTypedStoreCodec[F[_]: Sync, K, V](
       pairsBitVector <- kvPairs.toVector.traverse {
                          case (k, v) => encodeKey(k).map2(encodeValue(v))((x, y) => (x, y))
                        }
-      pairs = pairsBitVector.map { case (k, v) => (k.toByteVector.toDirectByteBuffer, v) }
-      _     <- store.put[BitVector](pairs, _.toByteVector.toDirectByteBuffer)
+      pairs = pairsBitVector.map { case (k, v) => (k.toDirectByteBuffer, v) }
+      _     <- store.put[ByteVector](pairs, _.toDirectByteBuffer)
     } yield ()
 
   override def delete(keys: Seq[K]): F[Int] =
     for {
       keysBitVector <- keys.toVector.traverse(encodeKey)
-      keysBuf       = keysBitVector.map(_.toByteVector.toDirectByteBuffer)
+      keysBuf       = keysBitVector.map(_.toDirectByteBuffer)
       deletedCount  <- store.delete(keysBuf)
     } yield deletedCount
 
   override def contains(keys: Seq[K]): F[Seq[Boolean]] =
     for {
       keysBitVector <- keys.toVector.traverse(encodeKey)
-      keysBuf       = keysBitVector.map(_.toByteVector.toDirectByteBuffer)
+      keysBuf       = keysBitVector.map(_.toDirectByteBuffer)
       results       <- store.get(keysBuf, _ => ())
     } yield results.map(_.nonEmpty)
 
   override def toMap: F[Map[K, V]] =
     for {
       valuesBytes <- store.iterate(
-                      _.map { case (k, v) => (BitVector(k), BitVector(v)) }.toVector
+                      _.map { case (k, v) => (ByteVector.view(k), ByteVector.view(v)) }.toVector
                     )
       values <- valuesBytes.traverse {
                  case (k, v) =>
